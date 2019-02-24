1380a01dfe2fc4ce076f319a88fa65f31eb6780e
Mohsen S <Mohsen.Shojaeifar@lamresearch.com>
2019-02-24 01:00:46 -0800

    fron-end

---
+++ assets/javascript/app.js
@@ -0,0 +1,72 @@
+var questionArr = new Array();
+questionArr[0] = new Array();
+questionArr[0][0] = "Question 1";
+questionArr[0][1] = "Answer 1";
+questionArr[0][2] = "Answer 2";
+questionArr[0][3] = "Answer 3";
+questionArr[0][4] = "Answer 4";
+questionArr[0][5] = "Correct Answer";
+
+questionArr[1] = new Array();
+questionArr[1][0] = "Question 2";
+questionArr[1][1] = "Answer 1";
+questionArr[1][2] = "Answer 2";
+questionArr[1][3] = "Answer 3";
+questionArr[1][4] = "Answer 4";
+questionArr[1][5] = "Correct Answer";
+
+questionArr[2] = new Array();
+questionArr[2][0] = "Question 2";
+questionArr[2][1] = "Answer 1";
+questionArr[2][2] = "Answer 2";
+questionArr[2][3] = "Answer 3";
+questionArr[2][4] = "Answer 4";
+questionArr[2][5] = "Correct Answer";
+
+questionArr[3] = new Array();
+questionArr[3][0] = "Question 2";
+questionArr[3][1] = "Answer 1";
+questionArr[3][2] = "Answer 2";
+questionArr[3][3] = "Answer 3";
+questionArr[3][4] = "Answer 4";
+questionArr[3][5] = "Correct Answer";
+
+questionArr[4] = new Array();
+questionArr[4][0] = "Question 2";
+questionArr[4][1] = "Answer 1";
+questionArr[4][2] = "Answer 2";
+questionArr[4][3] = "Answer 3";
+questionArr[4][4] = "Answer 4";
+questionArr[4][5] = "Correct Answer";
+
+questionArr[5] = new Array();
+questionArr[5][0] = "Question 2";
+questionArr[5][1] = "Answer 1";
+questionArr[5][2] = "Answer 2";
+questionArr[5][3] = "Answer 3";
+questionArr[5][4] = "Answer 4";
+questionArr[5][5] = "Correct Answer";
+
+questionArr[6] = new Array();
+questionArr[6][0] = "Question 2";
+questionArr[6][1] = "Answer 1";
+questionArr[6][2] = "Answer 2";
+questionArr[6][3] = "Answer 3";
+questionArr[6][4] = "Answer 4";
+questionArr[6][5] = "Correct Answer";
+
+questionArr[7] = new Array();
+questionArr[7][0] = "Question 2";
+questionArr[7][1] = "Answer 1";
+questionArr[7][2] = "Answer 2";
+questionArr[7][3] = "Answer 3";
+questionArr[7][4] = "Answer 4";
+questionArr[7][5] = "Correct Answer";
+
+questionArr[8] = new Array();
+questionArr[8][0] = "Question 2";
+questionArr[8][1] = "Answer 1";
+questionArr[8][2] = "Answer 2";
+questionArr[8][3] = "Answer 3";
+questionArr[8][4] = "Answer 4";
+questionArr[8][5] = "Correct Answer";
